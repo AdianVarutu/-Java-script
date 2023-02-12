ed7a63e421ed38e158c244b763e4a99a1a30ecee
Adrian Varutu <118045002+AdianVarutu@users.noreply.github.com>
2023-02-12 10:54:00 +0200

    operatii

---
+++ Un proiect in javascript.js
@@ -0,0 +1,17 @@
+
+console.log("Welcome to Programiz!");
+const nr1Text = prompt("enter number1:")
+const nr1 = parseInt(nr1Text);
+const nr2 = parseInt(prompt("enter number2:"));
+const op = prompt("operation:")
+if (op=="adunare" || op =="+") {
+    console.log(nr1+nr2)
+} else if(op=="-") {
+    console.log(nr1-nr2)
+} else if(op=="*") {
+    console.log(nr1*nr2)
+} else if(op=="/") {
+    console.log(Math.floor(nr1/nr2) + " rest " + nr1%nr2)
+} else if(op=="p") {
+    console.log(nr1**nr2)
+}
